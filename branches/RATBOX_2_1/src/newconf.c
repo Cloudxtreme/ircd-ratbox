4e54189a7158ecd972f7ade4c9d1d771f484bb3b
leeh <leeh@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2005-09-15 18:22:47 +0000

    - fix shared/cluster flags not being initialised to 0
    git-svn-id: http://svn.ratbox.org/svnroot/ircd-ratbox@20768 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- branches/RATBOX_2_1/src/newconf.c
+++ branches/RATBOX_2_1/src/newconf.c
@@ -1073,7 +1073,7 @@ static void
 conf_set_shared_flags(void *data)
 {
 	conf_parm_t *args = data;
-	int flags;
+	int flags = 0;
 	dlink_node *ptr, *next_ptr;
 
 	if(yy_shared != NULL)
@@ -1320,7 +1320,7 @@ static void
 conf_set_cluster_flags(void *data)
 {
 	conf_parm_t *args = data;
-	int flags;
+	int flags = 0;
 	dlink_node *ptr, *next_ptr;
 
 	if(yy_shared != NULL)
