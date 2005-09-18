896856806f2b3755271f8b37bfef2a86dc62e349
androsyn <androsyn@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2005-09-18 01:38:08 +0000

    don't remove oper only umodes for remote clients on deoper - from jilles
    git-svn-id: http://svn.ratbox.org/svnroot/ircd-ratbox@20776 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- branches/RATBOX_2_1/src/s_user.c
+++ branches/RATBOX_2_1/src/s_user.c
@@ -911,12 +911,12 @@ user_mode(struct Client *client_p, struct Client *source_p, int parc, const char
 						break;
 
 					ClearOper(source_p);
-					source_p->umodes &= ~ConfigFileEntry.oper_only_umodes;
 
 					Count.oper--;
 
 					if(MyConnect(source_p))
 					{
+						source_p->umodes &= ~ConfigFileEntry.oper_only_umodes;
 						source_p->flags2 &= ~OPER_FLAGS;
 
 						MyFree(source_p->localClient->opername);
