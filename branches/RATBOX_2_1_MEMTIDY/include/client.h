74b95153993e4c50bb318cfaf30fc3b2df9ddfb5
leeh <leeh@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2005-11-08 19:39:21 +0000

    - remove unused CIPHERKEYLEN
    git-svn-id: http://svn.ratbox.org/svnroot/ircd-ratbox@21120 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- branches/RATBOX_2_1_MEMTIDY/include/client.h
+++ branches/RATBOX_2_1_MEMTIDY/include/client.h
@@ -41,7 +41,6 @@
 /* we store ipv6 ips for remote clients, so this needs to be v6 always */
 #define HOSTIPLEN	53	/* sizeof("ffff:ffff:ffff:ffff:ffff:ffff:255.255.255.255.ipv6") */
 #define PASSWDLEN       128
-#define CIPHERKEYLEN    64	/* 512bit */
 #define CLIENT_BUFSIZE 512	/* must be at least 512 bytes */
 
 #define IDLEN		10
