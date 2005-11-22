a4424ef652602cd75ca7d3e6e829514592d36d42
leeh <leeh@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2005-11-22 19:02:34 +0000

    - rework the way trace sends out TRACEOPER/TRACEUSER and the spoof ip   hiding in it
    git-svn-id: http://svn.ratbox.org/svnroot/ircd-ratbox@21285 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- branches/RATBOX_2_2/modules/m_trace.c
+++ branches/RATBOX_2_2/modules/m_trace.c
@@ -380,27 +380,16 @@ report_this_status(struct Client *source_p, struct Client *target_p,
 		    (MyClient(source_p) || !(dow && IsInvisible(target_p))))
 		   || !dow || IsOper(target_p) || (source_p == target_p))
 		{
-			if(IsOper(target_p))
-				sendto_one_numeric(source_p, RPL_TRACEOPERATOR,
-						   form_str(RPL_TRACEOPERATOR),
-						   class_name, name,
-#ifndef HIDE_SPOOF_IPS
-						   MyOper(source_p) ? ip :
-#endif
-						   (IsIPSpoof(target_p) ? "255.255.255.255" : ip),
-						   CurrentTime - target_p->localClient->lasttime,
-						   CurrentTime - target_p->localClient->last);
+			int tnumeric = RPL_TRACEUSER;
 
-			else
-				sendto_one_numeric(source_p, RPL_TRACEUSER, 
-						   form_str(RPL_TRACEUSER),
-						   class_name, name,
-#ifndef HIDE_SPOOF_IPS
-						   MyOper(source_p) ? ip :
-#endif
-						   (IsIPSpoof(target_p) ? "255.255.255.255" : ip),
-						   CurrentTime - target_p->localClient->lasttime,
-						   CurrentTime - target_p->localClient->last);
+			if(IsOper(target_p))
+				tnumeric = RPL_TRACEOPERATOR;
+
+			sendto_one_numeric(source_p, tnumeric, form_str(tnumeric),
+					class_name, name,
+					show_ip(source_p, target_p) ? ip : "255.255.255.255",
+					CurrentTime - target_p->localClient->lasttime,
+					CurrentTime - target_p->localClient->last);
 			cnt++;
 		}
 		break;
