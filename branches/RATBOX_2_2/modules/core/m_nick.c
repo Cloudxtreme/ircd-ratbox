fc37e9910e16c6894489fe8227965327d755af1a
jilles <jilles@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2007-04-22 11:36:48 +0000

    Backport trunk r23845: Force nickTS to increase when a local user changes their nick.
    git-svn-id: http://svn.ratbox.org/svnroot/ircd-ratbox@23847 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- branches/RATBOX_2_2/modules/core/m_nick.c
+++ branches/RATBOX_2_2/modules/core/m_nick.c
@@ -691,7 +691,11 @@ change_local_nick(struct Client *client_p, struct Client *source_p, char *nick,
         /* dont reset TS if theyre just changing case of nick */
         if(!samenick)
 	{
-                source_p->tsinfo = CurrentTime;
+		/* force the TS to increase -- jilles */
+		if (source_p->tsinfo >= CurrentTime)
+			source_p->tsinfo++;
+		else
+			source_p->tsinfo = CurrentTime;
 		monitor_signoff(source_p);
 		/* we only do bancache for local users -- jilles */
 		if (source_p->user)
