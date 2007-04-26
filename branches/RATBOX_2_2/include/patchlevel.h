6f5963d4c5221a6740e940e43fa6c72a3ec9d8d3
leeh <leeh@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2007-04-26 18:28:10 +0000

    - update RELNOTES - revved patchlevel to 2.2.5
    git-svn-id: http://svn.ratbox.org/svnroot/ircd-ratbox@23881 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- branches/RATBOX_2_2/include/patchlevel.h
+++ branches/RATBOX_2_2/include/patchlevel.h
@@ -25,8 +25,8 @@
  */
 
 #ifndef PATCHLEVEL
-#define PATCHLEVEL   "ircd-ratbox-2.2.4"
-#define PATCHLEVEL_NUM	0202040500
+#define PATCHLEVEL   "ircd-ratbox-2.2.5"
+#define PATCHLEVEL_NUM	0202050500
 
 /* patchlevel numbers:
  * <major><minor><point><status><statusnum>
