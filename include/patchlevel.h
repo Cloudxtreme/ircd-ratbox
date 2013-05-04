2adb1ebb3379d1f29385ff2025e70cfa347c1622
dcorbe <corbe@corbe.net>
2013-05-04 19:16:57 +0300

    IRC proto-breaking characters have been reverted back to filtered.

--- include/patchlevel.h
+++ include/patchlevel.h
@@ -25,7 +25,7 @@
  */
 
 #ifndef PATCHLEVEL
-#define PATCHLEVEL   "ircd-ratbox-3.0.8+unicode1"
+#define PATCHLEVEL   "ircd-ratbox-3.0.8+unicode2"
 #define PATCHLEVEL_NUM	0300060500
 
 /* patchlevel numbers:
